bc7dc0db3704922f08cfa7829f9cdea6fa527b57
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-02-19 08:13:05 +1000

    домашка 19

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+link1:
+Console.WriteLine("Введите пятизначное число");
+int a = int.Parse(Console.ReadLine());
+int b = a;
+if (a < 10000 || a > 99999)
+{
+    Console.WriteLine("Вы ввели не корректное число");
+    goto link1;
+}
+int[] array = new int[5];
+for (int i = 4; i >= 0; i--) 
+{
+    array[i] = a % 10;
+    a /= 10;
+}
+if (array[0] == array[4] && array[1] == array[3])
+{ Console.WriteLine("Число " + b + "  палиндром");}
+else{ Console.WriteLine("Число " + b + "  непалиндром");}
